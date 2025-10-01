7ba46addeda41c8fa7a68a2494af2dd9f0fc3bae
shweta-git25 <itsshweta2025@gmail.com>
2025-10-01 17:30:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0886832e6b5c3b9e2"
+ami = "ami-0360c520857e3138f"
 instance_type = "t3.micro"
 key_name = "terraform-key"
 vpc_security_group_ids = ["sg-02c77fa93da5c771e"]
